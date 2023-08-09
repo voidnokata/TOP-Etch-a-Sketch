bd2add21ba690a3fa0737ca0f94b527b1c71e3e7
voidnokata <voidnokata@gmail.com>
2023-08-09 16:35:40 +0300

    Slider and buttons added

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const gridcontainer = document.getElementById("grid");
+const gridSizeSlider = document.getElementById("gridSize");
+const gridSizeValue = document.getElementById("gridSizeValue");
+const resetButton = document.getElementById("resetButton");
+const blackButton = document.getElementById("blackButton");
+
 gridcontainer.style.boxSizing = "border-box";
-let gridnumber = 10;
-const cellSize = 100 / gridnumber; // Calculating how much space a single cell should take over %100 portion
+
+let gridnumber = parseInt(gridSizeSlider.value);// Changing grid size according to slider
+let cellSize = 100 / gridnumber; // Calculating how much a single cell will take space over a %100 portion inside of its container
+let useBlackColor = false;
+let isColoring = false;
+let lastCellColored = null; 
 
 function getRandomColor() {
     const randomRed = Math.floor(Math.random() * 256);
@@ -10,17 +19,55 @@ function getRandomColor() {
     return `rgb(${randomRed},${randomGreen},${randomBlue})`;
 }
 
-for (let i = 0; i < gridnumber * gridnumber; i++) { // Creating the cells and appending to the container
-    const cell = document.createElement("div");
-    cell.setAttribute("class", "cell");
-    cell.style.backgroundColor = "white";
-    cell.style.border = "1px solid black";
-    cell.style.boxSizing = "border-box";
-    cell.style.width = `${cellSize}%`;
-    cell.style.height = `${cellSize}%`;
-    cell.addEventListener("mouseover", () => { // Adding event listener to each cell for random coloring on mouseover
-        const randomColor = getRandomColor();
-        cell.style.backgroundColor = randomColor;
-    });
-    gridcontainer.appendChild(cell);
+function createGrid() {
+    gridcontainer.innerHTML = ''; // Removing the older cells after changing the grid size
+    for (let i = 0; i < gridnumber * gridnumber; i++) {
+        const cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.style.backgroundColor = "white";
+        cell.style.border = "1px solid black";
+        cell.style.boxSizing = "border-box";
+        cell.style.width = `${cellSize}%`;
+        cell.style.height = `${cellSize}%`;
+
+        cell.addEventListener("mousedown", () => {
+            isColoring = !isColoring;
+            if (isColoring) {
+                lastCellColored = cell;
+                colorCell(cell);
+            }
+        });
+
+        cell.addEventListener("mousemove", () => {
+            if (isColoring && cell !== lastCellColored) {
+                lastCellColored = cell;
+                colorCell(cell);
+            }
+        });
+        gridcontainer.appendChild(cell);
+    }
 }
+
+function colorCell(cell) {
+    const color = useBlackColor ? "black" : getRandomColor();
+    cell.style.backgroundColor = color;
+}
+
+createGrid();
+
+gridSizeSlider.addEventListener("input", () => { // Updating the grid size when slider value changes
+    gridnumber = parseInt(gridSizeSlider.value);
+    gridSizeValue.textContent = `Grid size: ${gridnumber}x${gridnumber}`;
+    cellSize = 100 / gridnumber;
+    createGrid();
+});
+
+resetButton.addEventListener("click", () => { // Resetting the grid by running createGrid function
+    createGrid();
+});
+
+blackButton.addEventListener("click", () => { // Changing the boolean value of useBlackColor when clicked the button
+    useBlackColor = !useBlackColor;
+    blackButton.classList.toggle("active", useBlackColor);
+});
+
